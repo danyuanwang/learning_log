15a0db2aeac57fd468914bc06896f52f559e30e3
Danyuan Wang <danyuanwang@hotmail.com>
2019-02-07 23:17:53 -0800

    add azure domain allowed host

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+n$6@b@tj+#^6u)bfqs_!*)i*9k(p5ls5l7_r@)e1hva(1s*ex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xuanwugame.azurewebsites.net']
 
 
 # Application definition
